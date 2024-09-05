ceec92523dcbdc777f02cf8c2494f71a40ac27b7
Abdumalik Umarov <63663677+loki0105@users.noreply.github.com>
2024-09-05 17:11:51 +0900

    Update index.js
    added a new feature  that allows to choose  to convert long url to shorter one or to qr-code

--- index.js
+++ index.js
@@ -2,53 +2,54 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import Darkmode from "darkmode-js";
+
+import qr from "qr-image";
+import fs from "fs";
+
 const app = express();
 app.use(express.static("public")); // Serve static files from the 'public' directory
 const port = 3000;
 const API_URL = "https://cleanuri.com/api/v1/shorten";
 app.use(bodyParser.urlencoded({ extended: false }));
 new Darkmode().showWidget();
- 
-
 
 app.set("view engine", "ejs"); // Set the view engine to EJS
 
 app.get("/", async (req, res) => {
-	res.render("index.ejs", { dat1: 0});
+	res.render("index.ejs", { dat1: 0 });
 });
 
 app.post("/submit", async (req, res) => {
 	const ds = req.body.links;
 	const data = new URLSearchParams({ url: ds }).toString();
+	console.log(req.body.sel1);
 
-	try {
-		const response = await axios.post(API_URL, data, {
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-		});
-		 
-		res.render("index.ejs", { dat1: response.data.result_url });
+	if (req.body.sel1 === "1") {
+		try {
+			const response = await axios.post(API_URL, data, {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			});
+			console.log("done");
+			res.render("index.ejs", { dat1: response.data.result_url });
+		} catch (error) {
+			res.render("index.ejs", { dat1: error.status });
+		}
+	} else {
+		try {
+			var qr_svg = qr.image(req.body.links);
+			qr_svg.pipe(fs.createWriteStream("public/qr-of-link.png"));
+			console.log("done2");
+			res.render("index.ejs", { dat1: 3 });
+		} catch (error) {
+			res.render("index.ejs", { dat1: error.status });
+		}
 
-
-    } catch (error) {
-  
-        res.render("index.ejs", { dat1: error.status });
-	
-    }
-    
-
-  
-
-	
+		
+	}
 });
 
 app.listen(port, () => {
 	console.log("Listening on port", port);
 });
-
-
-
-
-
-	
